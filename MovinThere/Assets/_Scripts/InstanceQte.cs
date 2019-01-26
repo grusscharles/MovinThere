85abfff1613c53d578f60d43cda748a5bd914891
jordasil <joris.dasilva@gmail.com>
2019-01-26 15:02:00 +0100

    ajout scene experimentale pour qte

---
+++ MovinThere/Assets/_Scripts/InstanceQte.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstanceQte : MonoBehaviour {
+
+
+    private GameObject nouveauCanvas;
+    public GameObject[] mesBoutons;
+    private GameObject nouveauBouton;
+    private int boutonRandom;
+    private float tempsDattente;
+    private float nouveauTempsDattente = 2f;
+     
+    // Use this for initialization
+	void Start () {
+
+        nouveauCanvas = GameObject.Find("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Time.time > tempsDattente)
+        {
+            boutonRandom = Random.Range(0, 3);
+            //print("le bouton instantié est le numéro" + boutonRandom);
+
+            nouveauBouton = Instantiate(mesBoutons[boutonRandom]) as GameObject;
+            nouveauBouton.transform.SetParent(nouveauCanvas.transform, false);
+            Destroy(nouveauBouton, nouveauTempsDattente);
+
+            tempsDattente = Time.time + nouveauTempsDattente;
+        }
+
+        if(nouveauBouton == mesBoutons[0])
+        {
+            print("le bouton instancié est le numéro 1");
+        }
+
+        if (nouveauBouton == mesBoutons[1])
+        {
+            print("le bouton instancié est le numéro 2");
+        }
+        if (nouveauBouton == mesBoutons[2])
+        {
+            print("le bouton instancié est le numéro 3");
+        }
+        if (nouveauBouton == mesBoutons[3])
+        {
+            print("le bouton instancié est le numéro 4");
+        }
+    }
+}
