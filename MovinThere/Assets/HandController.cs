c49de34f65ec921b8069bad408e0ed748473ee4f
grusscharles <grusscharles@gmail.com>
2019-01-26 19:38:31 +0100

    scene carton ranger

---
+++ MovinThere/Assets/HandController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+    [SerializeField]
+    Transform battant;
+
+    SpriteSwap spriteSwap;
+
+    private void Awake()
+    {
+        spriteSwap = GetComponent<SpriteSwap>();
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Grab();
+	}
+
+    void Grab()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        Debug.Log(h);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spriteSwap.SwapSprite();
+            battant.Rotate(new Vector3(h,0,0));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ok");
+    }
+}
---
+++ MovinThere/Assets/_Scripts/SpriteSwap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwap : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Sprites list")]
+    List<Sprite> sprites;
+
+    int currentSpriteIndex;
+    SpriteRenderer sprtRend;
+
+    private void Awake()
+    {
+        sprtRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        sprtRend.sprite = sprites[0];
+        currentSpriteIndex = 0;
+    }
+
+    public void SwapSprite()
+    {
+        if (currentSpriteIndex != sprites.Count - 1)
+        {
+            sprtRend.sprite = sprites[currentSpriteIndex + 1];
+            currentSpriteIndex += 1;
+        }
+        else
+        {
+            sprtRend.sprite = sprites[0];
+            currentSpriteIndex = 0;
+        }
+    }
+}
+	
