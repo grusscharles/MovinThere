d192985d698ec232be3cb346d4d589b54661620d
CH1M37 <CH1M37>
2019-01-26 17:32:08 +0100

    test friction

---
+++ MovinThere/Assets/_Scripts/BoxFriction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFriction : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    Rigidbody2D rbBox;
+    public float frictionCoef = 1.0f;
+    private Vector2 vitesse;
+
+    private void Awake()
+    {
+        rbBox = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+
+        vitesse = rb.velocity;
+
+        Debug.Log(vitesse);
+
+        rbBox.AddForce(vitesse);
+	}
+}
