7056023aa7fe242bd2763a4b77295b020a05ad8a
grusscharles <grusscharles@gmail.com>
2019-01-26 12:15:57 +0100

    script floor

---
+++ MovinThere/Assets/_Scripts/Floor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+    [SerializeField]
+    Collider2D floorCol;
+
+    void Start () {
+        floorCol.enabled = false;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        floorCol.enabled = true;
+    }
+
+    void Update () {
+		
+	}
+}
