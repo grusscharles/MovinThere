f8b544a2d8fe739bd2bb97289c77458f3ebf33ba
grusscharles <grusscharles@gmail.com>
2019-01-26 16:14:47 +0100

    Camera Follows Player

---
+++ MovinThere/Assets/CameraBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    bool followPlayer = false;
+    Vector3 offset;
+    PlayerController2D player;
+    float camXPos, camZPos;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerController2D>();
+        camXPos = transform.position.x;
+        camZPos = transform.position.z;
+    }
+
+	
+	void Update () {
+
+        if (followPlayer)
+        {
+            FollowPlayer();
+        }
+	}
+
+    public void SetCam()
+    {
+        offset = transform.position - player.transform.position;
+        followPlayer = true;
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = new Vector3(camXPos, player.transform.position.y + offset.y, camZPos);
+    }
+}
+
