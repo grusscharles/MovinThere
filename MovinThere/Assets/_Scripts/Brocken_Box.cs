3641f7a75b36773a332129fda35a8f0a9a5af24c
CH1M37 <CH1M37>
2019-01-27 19:23:57 +0100

    tests passation

--- MovinThere/Assets/_Scripts/Brocken_Box.cs
+++ MovinThere/Assets/_Scripts/Brocken_Box.cs
@@ -6,16 +6,17 @@ public class Brocken_Box : MonoBehaviour {
 
     public GameObject brokenBox;
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Box")
+        if (collision.gameObject.tag == "Box" && enabled == true)
         {
             Debug.Log("destruction");
             Vector3 pos = collision.gameObject.transform.position;
 
             Destroy(collision.gameObject);
 
-            GameObject instance = Instantiate(brokenBox, pos, Quaternion.identity);
+            Instantiate(brokenBox, pos, Quaternion.identity);
         }
     }
 
