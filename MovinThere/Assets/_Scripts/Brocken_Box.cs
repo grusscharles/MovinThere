770013ae83e7a1b10adb5036b8abb94c44a4005c
CH1M37 <CH1M37>
2019-01-27 14:56:35 +0100

    ajout du tag BOX et des box qui se detruisent

---
+++ MovinThere/Assets/_Scripts/Brocken_Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brocken_Box : MonoBehaviour {
+
+    public GameObject brokenBox;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Box")
+        {
+            Vector3 pos = collision.gameObject.transform.position;
+
+            Destroy(collision.gameObject);
+
+            GameObject instance = Instantiate(brokenBox, pos, Quaternion.identity);
+        }
+    }
+}
