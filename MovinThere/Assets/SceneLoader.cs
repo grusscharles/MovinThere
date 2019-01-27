fa29b14e84cd75e5df667b0da5f82f504ab74747
grusscharles <grusscharles@gmail.com>
2019-01-27 18:02:26 +0100

    Scene Loader et build settings

---
+++ MovinThere/Assets/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Name of the scene to load")]
+    string sceneName;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
