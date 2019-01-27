4f6a274e7304d7d3f0fda10062d6d9b8b6f5af39
CH1M37 <CH1M37>
2019-01-27 17:57:31 +0100

    tests passation de box

---
+++ MovinThere/Assets/_Scripts/Box_Pass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Pass : MonoBehaviour {
+
+	void Update () {
+
+        /*
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Box")
+            {
+                Vector3 pos = collision.gameObject.transform.position;
+
+                Destroy(collision.gameObject);
+            }
+        }
+        */
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            transform.position = new Vector3 (5, 5, 5);
+        }
+		
+	}
+}
